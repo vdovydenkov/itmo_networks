84dbb18bc8a1fc5b6d41a0e98a2594259cf9d0b1
VDovydenkov <dov@mlbox.ru>
2025-06-24 04:45:28 +0300

    Удалил лишний подключаемый модуль, т.к. не используется.

--- dns_monitor/app/db.py
+++ dns_monitor/app/db.py
@@ -1,6 +1,6 @@
 import psycopg2
-from psycopg2.extras import RealDictCursor
 from app.config import Settings
 
 def get_connection():
-    return psycopg2.connect(Settings.DB_DSN, cursor_factory=RealDictCursor)
+    # return psycopg2.connect(Settings.DB_DSN, cursor_factory=RealDictCursor)
+    return psycopg2.connect(Settings.DB_DSN)
